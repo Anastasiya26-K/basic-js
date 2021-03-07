62acbca74b65e9d447e502e4cc917c1c19183af6
Anastasiya Karotkaya <76777872+Anastasiya26-K@users.noreply.github.com>
2021-03-07 13:01:46 +0300

    fix: finishChain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,35 +1,38 @@
-const CustomError = require("../extensions/custom-error");
-
 const chainMaker = {
-    newArr = [],
+  newArr: [],
+
   getLength() {
     return this.newArr.length;
   },
+
   addLink(value) {
     this.newArr.push(value);
-      return this;
+    return this;
   },
+
   removeLink(position) {
-      if (position < 1 || typeof(position)!="number") {
-        this.newArr = [];
-        throw Error();
-          }
-          this.newArr.splice(position - 1, 1);
-          return this;
-        },
-  reverseChain() {
-    this.newArr.reverse()
-    return this
-  },
-  finishChain() {
-    for (let i = 0; i < this.myArr.length; i++) {
-      if (i !== this.chain.length - 1) {
-    result = this.newArr.join('~~');
-       this.newArr.splice(0, this.myArr.length)
-      }
+    if (position < 1 || typeof(position)!="number") {
+      this.newArr = []
+      throw Error();
     }
-    return result
-      }
+    this.newArr.splice(position - 1, 1);
+    return this;
+  },
+
+  reverseChain() {
+    this.newArr.reverse();
+    return this;
+  },
+
+  finishChain() {
+    let theChain = this.newArr.join('~~');
+    this.newArr.splice(0, this.newArr.length)
+    return theChain;
+  }
 };
 
 module.exports = chainMaker;
+
+
+
+
